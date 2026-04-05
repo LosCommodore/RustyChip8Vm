453a27b69f5ac23d71fa27850ad3443216139861
Christian Langen <christian.langen@gmail.com>
2026-04-05 08:36:07 +0200

    command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use anyhow::{Context, Result};
 use rusty_chip8_vm::chip8::Chip8;
 use rusty_chip8_vm::terminal_screen::TerminalScreen;
-use std::fs;
+use std::{env, fs, process};
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Missing parameter: progam file");
+        process::exit(1);
+    }
+
+    let file = &args[1];
     let screen = TerminalScreen::new()?;
-
-    //let file = "chip_test/chip8-test-rom/test_opcode.ch8";
-    //let file = "chip_test/chip8-test-rom-2/chip8-test-rom.ch8";
-
-    let file = "games/INVADERS";
+    //let file = "games/INVADERS";
     let program = fs::read(file).with_context(|| format!("Could not read file: {}", file))?;
     let mut chip8 = Chip8::new(&program, screen);
     chip8.run()?;
